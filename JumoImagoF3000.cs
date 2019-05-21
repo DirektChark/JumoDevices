e390a1ef87bac4584b03377f14e7185e826dde8f
Jon Cederqvist <jon.cederqvist@direktchark.se>
2019-05-21 15:57:14 +0200

    Initial commit

---
+++ JumoImagoF3000.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ModbusDirekt;
+
+namespace JumoDevices
+{
+    public class JumoImagoF3000
+    {
+        public ModbusClient Client { get; }
+
+        public JumoImagoF3000(ModbusDirekt.ModbusClient client)
+        {
+            this.Client = client;
+        }
+
+
+        public string GetVersion()
+        {
+            var reg1 = Client.ReadInputRegisters(0, 5).GetString();
+            var reg2 = Client.ReadInputRegisters(0x33, 2).GetString();
+
+            return reg1;
+        }
+
+        public float ReadFloat(int addr)
+        {
+            return Client.ReadInputRegisters(addr, 2).GetFloat();
+        }
+
+
+        public float ReadFilteredActualValue(int v)
+        {
+            return Client.ReadInputRegisters(0x5b+v*2, 2).GetFloat();
+        }
+        public float ReadModbusActualValue(int v)
+        {
+            return Client.ReadInputRegisters(0x71 + v*2, 2).GetFloat();
+        }
+
+        public float ReadTerminalTemp()
+        {
+            return Client.ReadInputRegisters(0x6b, 2).GetFloat();
+        }
+
+        public float ReadCalculatedHumidity()
+        {
+            return Client.ReadInputRegisters(0x6d, 2).GetFloat();
+        }
+
+        public float ReadActualCore()
+        {
+            return Client.ReadInputRegisters(0x6f, 2).GetFloat();
+        }
+        public float ReadTerminalTemp2()
+        {
+            return Client.ReadInputRegisters(0x81, 2).GetFloat();
+        }
+        public float ReadHumidity2()
+        {
+            return Client.ReadInputRegisters(0x83, 2).GetFloat();
+        }
+        public float ReadActualCore2()
+        {
+            return Client.ReadInputRegisters(0x85, 2).GetFloat();
+        }
+        public float ReadActualFValue()
+        {
+            return Client.ReadInputRegisters(0x87, 2).GetFloat();
+        }
+
+        public float ReadActualCValue()
+        {
+            return Client.ReadInputRegisters(0x89, 2).GetFloat();
+        }
+
+        
+
+        /// <summary>
+        /// Reads Modbus address 0x8B, float, r/o
+        /// (Prgram Source : setpoint chamber delta.)
+        /// </summary>
+        public float ReadSPChamberDelta()
+        {
+            return Client.ReadInputRegisters(0x8b, 2).GetFloat();
+        }
+
+        /// <summary>
+        /// Reads Modbus address 0x8D, float, r/o
+        /// (Prgram Source : setpoint humidity indicatin)
+        /// </summary>
+        public float ReadSPrHInd()
+        {
+            return Client.ReadInputRegisters(0x8D, 2).GetFloat();
+        }
+
+        /// <summary>
+        /// Reads Modbus address 0x8F, float, r/o
+        /// (Prgram Source : setpoint chamber)
+        /// </summary>
+        public float ReadSPChamber()
+        {
+            return Client.ReadInputRegisters(0x8F, 2).GetFloat();
+        }
+
+        public float ReadSPrH()
+        {
+            return Client.ReadInputRegisters(0x91, 2).GetFloat();
+        }
+
+        /// <summary>
+        /// Reads Modbus address 0x93, float, r/o
+        /// (Prgram Source : setpoint core)
+        /// </summary>
+        public float ReadSPCore()
+        {
+            return Client.ReadInputRegisters(0x93, 2).GetFloat();
+        }
+
+
+        public string ReadBinaryAlarmText()
+        {
+            return TrimData(Client.ReadInputRegisters(0x0241, 21).GetString());
+        }
+        public string ReadProgramName()
+        {
+            string data = Client.ReadInputRegisters(0x0257, 17).GetString();
+
+            return TrimData(data);
+        }
+        public string ReadProcessName()
+        {
+            string data = Client.ReadInputRegisters(0x0260, 17).GetString();
+            return TrimData(data);
+        }
+        public string ReadNextProcessName()
+        {
+            return TrimData(Client.ReadInputRegisters(0x0269, 17).GetString());
+        }
+
+        public string ReadAllProdDataText()
+        {
+            var sb = new StringBuilder();
+            string data = "";
+            for(int i = 0; i < 8; i++)
+            {
+                data = Client.ReadInputRegisters(0x0272 + i * 17, 17).GetString();
+                sb.Append(data);
+
+            }
+
+            return sb.ToString();
+        }
+        private static string TrimData(string data)
+        {
+            return data.Substring(0, data.IndexOf('\0'));
+        }
+
+        /// <summary>
+        /// Reads Modbus address 0xAE, word, r/w
+        /// (Prgram Source : actual program-no.)
+        /// </summary>
+        public int ReadProgramNo()
+        {
+            return (int)Client.ReadInputRegisters(0xAE, 1).GetUint16();
+        }
+
+        /// <summary>
+        /// Reads Modbus address 0xAF, word, r/o
+        /// (Prgram Source : actual section-no.)
+        /// </summary>
+        public int ReadStepNo()
+        {
+            return (int)Client.ReadInputRegisters(0xAF, 1).GetUint16();
+        }
+
+        /// <summary>
+        /// Reads Modbus address 0xB0, word, r/o
+        /// (Prgram Source : max. nsection-no.)
+        /// </summary>
+        public int ReadStepCount()
+        {
+            return (int)Client.ReadInputRegisters(0xB0, 1).GetUint16();
+        }
+    }
+}
---
+++ JumoWtrans.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ModbusDirekt;
+
+namespace JumoDevices
+{
+    public class JumoWtrans
+    {
+        public JumoWtrans(ModbusClient client, int unitId)
+        {
+            this.Client = client;
+        }
+
+        public ModbusClient Client { get; }
+
+        public string GetVersion()
+        {
+            var reg1 = Client.ReadInputRegisters(0, 5).GetString();
+            var reg2 = Client.ReadInputRegisters(0x33, 2).GetString();
+
+            return reg1;
+        }
+
+        public float ReadRawValue(int channel)
+        {
+            int valSize = 2;
+            int address = 0x67 + (channel - 1) * valSize;
+            var reg1 = Client.ReadInputRegisters(address, 2);
+
+            float val = reg1.GetFloat();
+            ValidateFloatValue(val);
+            return val;
+        }
+
+        public uint ReadUpdateTime(int channel)
+        {
+            var reg1 = Client.ReadInputRegisters(0x87 + (channel-1)*2, 2);
+            return reg1.GetUint32();
+        }
+
+        public ushort ReadTransmitInterval(int i)
+        {
+            var reg1 = Client.ReadInputRegisters(0xA7, 1);
+            return reg1.GetUint16();
+        }
+
+        public float ReadDisplayValue(int channel)
+        {
+            var addr = 0x00E7 + --channel *2;
+            var reg = Client.ReadInputRegisters(addr, 2);
+            var val = reg.GetFloat();
+            ValidateFloatValue(val);
+            return val;
+        }
+
+        public AlarmOutputValues ReadAlarmOutput(int channel)
+        {
+            channel--;
+            var reg = Client.ReadInputRegisters(0x0107 +channel, 1);
+            int alarmStatus = (int)reg.Data[0];
+            return (AlarmOutputValues)alarmStatus;
+        }
+
+        static void ValidateFloatValue(float input)
+        {
+            var val = input / 1e37;
+            string error = null;
+            switch (val)
+            {
+                case 1f: error = "Underrange"; break;
+                case 2f: error = "Overrange"; break;
+                case 3f: error = "No valid input value"; break;
+                case 4f:error = "Division by zero.";break;
+                case 5f: error = "Math error."; break;
+                case 6f: error = "Invalid terminal temperature of thermocouple."; break;
+                case 7f: error = "Still no minimum value (drag indicator)."; break;
+                case -7f: error = "Still no maximum value (drag indicator)."; break;
+                case 8f: error = "Integrator or statistics destroyed."; break;
+                case 9f: error = "Radio timeout"; break;
+            }
+            if(error != null)
+            {
+                throw new WTransErrorException(error);
+            }
+        }
+
+        public uint ReadPowerOnTime(int channel)
+        {
+            var addr = 0x03d5 + --channel * 2;
+            var reg = Client.ReadInputRegisters(addr, 2);
+            return reg.GetUint32();
+        }
+
+        public uint ReadTransmitterID(int channel)
+        {
+            var addr = 0x03B5 + --channel * 2;
+            var reg = Client.ReadHoldingRegisters(addr, 2);
+            return reg.GetUint32();
+        }
+    }
+
+    public enum AlarmOutputValues
+    {
+        NoAlarms = 0,
+        RadioTimeout = 1,
+        AlarmMonitoring1 = 2,
+        AlarmMonitoring2 = 4,
+        LowBattTransmittter = 8
+    }
+
+    class WTransErrorException : Exception
+    {
+        public WTransErrorException(string message) : base(message)
+        {
+        }
+    }
+}
